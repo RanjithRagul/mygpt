22241e27acc036f7876b56b14b1d8d841f97da2d
RanjithRagul <70192433+RanjithRagul@users.noreply.github.com>
2026-09-15 22:46:20 +0530

    add new line for config2

--- train.py
+++ train.py
@@ -40,6 +40,7 @@ weight_decay = 1e-1 # 0.1
 beta1 = 0.9
 beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
+
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
 warmup_iters = 2000
